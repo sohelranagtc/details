0fb9dd87136adb23c8a8c13aa110474e42de1eb3
sohelranagtc <sohelranagtc@gmail.com>
2023-03-11 16:03:59 +0600

    final

--- details.js
+++ details.js
@@ -31,23 +31,58 @@ const DeleteMessage = () =>{
     localStorage.removeItem('Message')
 }
 
+const getDataAll = () => {
+    let cart = {}
+    const SaveCart = localStorage.getItem('player')
+    if(SaveCart){
+        cart = JSON.parse(SaveCart)
+    }
+    return cart
+}
+
+
+const showAllData = (name, others) =>{
+    const Container = document.getElementById('container')
+    const p = document.createElement('p')
+    p.innerHTML = `${name} : ${others}`
+    Container.appendChild(p)
+}
+
 const SendAll = () => {
+    const cart = getDataAll()
+    const IdName = document.getElementById('name').innerText
     const GetName = document.getElementById('get-name')
     const Name = GetName.value
     GetName.value = ""
+    cart[IdName] = Name
 
+    const EmailID = document.getElementById('email').innerText
     const GetEmail = document.getElementById('get-email')
     const Email = GetEmail.value
     GetEmail.value = ""
+    cart[EmailID] = Email
 
+    const MessageID = document.getElementById('message').innerText
     const GetMessage = document.getElementById('get-message')
     const Message = GetMessage.value
     GetMessage.value = ""
+    cart[MessageID] = Message
 
+    const cartStringified = JSON.stringify(cart)
+    localStorage.setItem('player', cartStringified)
 }
 
+const DisplayAllMyData = () => {
+    const savedCart = getDataAll()
+    for(const cart in savedCart){
+        const others = savedCart[cart]
+        showAllData(cart, others)
+    }
+}
 
 
 const ResetAll = () => {
     localStorage.clear()
 }
+
+DisplayAllMyData ()
